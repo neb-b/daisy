482afcc29af3d21b74453a1fa5670802de4a3b52
neb <nebb_bb@protonmail.com>
2023-01-10 23:23:30 -0600

    fix background color

--- src/components/MessageInput.tsx
+++ src/components/MessageInput.tsx
@@ -1,35 +1,29 @@
 import { View } from "react-native"
-import {
-  Input,
-  Button,
-  Divider,
-  TopNavigation,
-  Layout,
-  Icon,
-  Text,
-  TopNavigationAction,
-} from "@ui-kitten/components"
+import { Input, Button, Divider, Icon, useTheme } from "@ui-kitten/components"
 
-export const MessageInput = () => (
-  <>
-    <Divider />
-    <View
-      style={{
-        display: "flex",
-        flexDirection: "row",
+export const MessageInput = () => {
+  const theme = useTheme()
 
-        padding: 10,
-      }}
-    >
-      <Input
-        style={{ borderRadius: 20, height: 40, flex: 1, marginRight: 8 }}
-        placeholder="Send a reply..."
-        textStyle={{ flex: 1, fontSize: 18 }}
-      />
-      <Button
-        style={{ height: 40, width: 40, borderRadius: 20 }}
-        accessoryLeft={(props) => <Icon {...props} name="paper-plane-outline" />}
-      />
+  return (
+    <View style={{ backgroundColor: theme["background-basic-color-3"] }}>
+      <Divider />
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          padding: 10,
+        }}
+      >
+        <Input
+          style={{ borderRadius: 20, height: 40, flex: 1, marginRight: 8 }}
+          placeholder="Send a reply..."
+          textStyle={{ flex: 1, fontSize: 18 }}
+        />
+        <Button
+          style={{ height: 40, width: 40, borderRadius: 20 }}
+          accessoryLeft={(props) => <Icon {...props} name="paper-plane-outline" />}
+        />
+      </View>
     </View>
-  </>
-)
+  )
+}
